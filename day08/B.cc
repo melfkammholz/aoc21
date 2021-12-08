f9db4ee6a7fee0ae8ddf400e96df5664b920a5eb
Melf Kammholz <melf.kammholz@gmail.com>
2021-12-08 08:04:55 +0100

    day08

---
+++ day08/B.cc
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+/* #include <atcoder/all> */
+
+using namespace std;
+
+using _int = int;
+#define int long long
+#define ll long long
+#define ull unsigned long long
+
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vvi vector<vector<int>>
+#define ii pair<int, int>
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define CONCAT_INNER(a, b) a ## b
+#define CONCAT(a, b) CONCAT_INNER(a, b)
+#define REP(n) FOR(CONCAT(_i, __COUNTER__), n)
+#define ALL(x) x.begin(), x.end()
+#define RALL(x) x.rbegin(), x.rend()
+#define SLC(x, l, r) x.begin() + l, x.begin() + r
+#define SUM(x) accumulate(ALL(x), 0LL)
+#define SORT(x) sort(ALL(x))
+#define MOD 1000000007
+#define EPS 10e-9
+#define DIR4 {{0,1}, {1,0}, {0,-1}, {-1,0}}
+#define DIR8 {{0,1}, {0,-1}, {1,0}, {-1,0}, {-1,-1}, {-1,1}, {1,-1}, {1,1}}
+#define pb push_back
+#define eb emplace_back
+#define fst first
+#define snd second
+#define mp make_pair
+#define mt make_tuple
+#define SZ(x) ((int) (x).size())
+#define MODM(x, m) (((x) % (m) + (m)) % (m))
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+  #define dout if (DEBUG) cout
+#ifdef DEBUG
+  #define debug(...) { \
+    __VA_ARGS__;       \
+  }                      
+#else
+  #define debug(...)
+#endif
+#define VAR(x) #x << "=" << x
+#define BIN16(x) bitset<16>(x)
+#define BIN32(x) bitset<32>(x)
+#define BIN64(x) bitset<64>(x)
+#define BIN(x) BIN32(x)
+#define OCT(x) oct << x << dec 
+#define HEX(x) hex << x << dec
+
+// remember: int = long long
+int clz(unsigned int x) {
+  return 8 * (int) sizeof(unsigned int) - 1 - __builtin_clzll(x);
+}
+
+int popcnt(unsigned int x) { return __builtin_popcountll(x); }
+
+template<typename T>
+bool chmin(T& a, const T b) { return a > b ? a = b, true : false; }
+
+template<typename T>
+bool chmax(T& a, const T b) { return a < b ? a = b, true : false; }
+
+template<typename T>
+T sgn(T a) { return (a > 0) - (a < 0); }
+
+struct skip {
+  int n;
+  skip(int _n): n(_n) {}
+  skip(const char* s) : n(strlen(s)) {}
+  friend istream &operator>>(istream &is, const skip &s) {
+    is.ignore(s.n);
+    return is;
+  }
+};
+
+/* ------------------------------------------------ */
+
+/* using mint = atcoder::modint998244353; */
+/* using mint = atcoder::modint1000000007; */
+
+void test_case() {
+  string s;
+
+  map<string, int> m1;
+  map<int, string> m2;
+
+  auto eraseChar = [](string& s, char c) {
+    s.erase(remove(ALL(s), c), s.end());
+  };
+
+  int r = 0;
+  while (getline(cin, s)) {
+    stringstream ss;
+    ss << s;
+    string t;
+    set<string> A, B;
+    while (ss >> t) {
+      SORT(t);
+      if (t.size() == 2) m1[t] = 1, m2[1] = t;
+      else if (t.size() == 4) m1[t] = 4, m2[4] = t;
+      else if (t.size() == 3) m1[t] = 7, m2[7] = t;
+      else if (t.size() == 7) m1[t] = 8, m2[8] = t;
+      else if (t.size() == 5) A.insert(t);
+      else if (t.size() == 6) B.insert(t);
+      else break;
+    }
+
+    auto deduce = [&](int a, int b, int c, set<string>& s) {
+      string v;
+      for (string u : s) {
+        string t = m2[a];
+        for (char c : u)
+          eraseChar(t, c);
+        if (SZ(t) == c) {
+          m1[u] = b;
+          m2[b] = u;
+          v = u;
+        }
+      }
+      s.erase(v);
+    };
+
+    deduce(7, 3, 0, A);
+    deduce(3, 9, 0, B);
+    deduce(9, 5, 1, A);
+    deduce(5, 2, 2, A);
+    deduce(5, 0, 1, B);
+    deduce(5, 6, 0, B);
+
+    int x = 0;
+    while (ss >> t) {
+        SORT(t);
+        x *= 10;
+        x += m1[t];
+    }
+    r += x;
+  }
+  cout << r << endl;
+}
+
+_int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  /* cout << fixed << setprecision(9); */ 
+  
+  int t = 1;
+  /* cin >> t; */
+  FOR(i, t) {
+    /* cout << "Case #" << (i + 1) << ": "; */
+    test_case();
+  }
+}
+
